b69edbebe9f3f58a6337187e5f8e0966124291a2
Anthony Wiggins <thewiggins21@gmail.com>
2024-07-09 01:49:22 -0400

    removed port 3000 from vite.config

--- vite.config.js
+++ vite.config.js
@@ -4,11 +4,11 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({ // Vite configuration
   plugins: [react()], 
-  build: {
-    outDir: 'dist', // Ensure this matches your Netlify publish directory
-  },
-  server: {
-    port: 3000,
-    open: true,
-  },
+  // build: {
+  //   outDir: 'dist', // Ensure this matches your Netlify publish directory
+  // },
+  // server: {
+  //   port: 3000,
+  //   open: true,
+  // }, 
 })
