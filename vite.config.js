494eaa8db8699b7496ecd7d8d9e3823671997598
Anthony Wiggins <thewiggins21@gmail.com>
2024-07-09 01:29:05 -0400

    deployed with netlify

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({ // Vite configuration
   plugins: [react()], 
-  server: {
-    port: 3000,
-    open: true,
+  build: {
+    outDir: 'dist', // Ensure this matches your Netlify publish directory
+  // server: {
+  //   port: 3000,
+  //   open: true,
   },
 })
